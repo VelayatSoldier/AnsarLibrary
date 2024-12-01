e220e35ce27f2f3ef4030660f6fda96ddbe8c284
Velayat Soldier <hgolestani13@gmail.com>
2024-12-01 17:39:13 +0330

    modified

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven{ setUrl("https://jitpack.io")}
+        maven{
+            setUrl("https://jitpack.io")
+            credentials { username = "jp_smmimeejq57smiu7kflrpv5i2d" }
+        }
     }
 }
 
